74a4a300392f1661d817b0f06833912fa80da113
bilal-surface51 <106622811+bilal-surface51@users.noreply.github.com>
2022-07-01 09:56:16 -0400

    first commit

--- Travel-Ajency-theme/page-about.php
+++ Travel-Ajency-theme/page-about.php
@@ -14,11 +14,7 @@
         <div class="container">
             <div class="top_text">
                 <h3>Our Story</h3>
-                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-                    labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-                    et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-                    labore et dolore magna aliquyam erat, sed diam voluptua. At vero eosum d</p>
+                <p</p>
             </div>
             <div class="row">
                 <div class="col-lg-6 col-md-8 m-auto">
